28049ae22f55601025273a60a0deb9f97d6a7d4c
sirinekaroui <sirine.karoui@esprit.tn>
2023-11-08 19:54:00 +0000

    update

--- src/test/java/tn/esprit/rh/achat/FactureMockitoTest.java
+++ src/test/java/tn/esprit/rh/achat/FactureMockitoTest.java
@@ -1,55 +1,67 @@
-
 package tn.esprit.rh.achat;
 
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import tn.esprit.rh.achat.controllers.FactureRestController;
 import tn.esprit.rh.achat.entities.Facture;
-import tn.esprit.rh.achat.repositories.FactureRepository;
-import tn.esprit.rh.achat.services.FactureServiceImpl;
+import tn.esprit.rh.achat.services.IFactureService;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import static com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat.UUID;
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-@RunWith( SpringRunner.class)
-@ContextConfiguration(classes = {FactureServiceImpl.class})
 public class FactureMockitoTest {
 
-    private FactureServiceImpl service;
-    private FactureRepository repository;
+    @InjectMocks
+    private FactureRestController factureController;
 
-    @Test
-    public void getFactureTest(){
-        System.out.println(" get test Facture");
-        long id = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+    @Mock
+    private IFactureService factureService;
 
-
-        List<Facture> FactureList = new ArrayList<>();
-
-        Facture facture1 = new Facture();
-        facture1.setIdFacture(id);
-        facture1.setMontantRemise(100); // Set the montantRemise to your desired value
-        facture1.setMontantFacture(500);
-
-        when(repository.findAll()).thenReturn(FactureList);
-
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
     }
 
-}
+    @Test
+    public void testGetFactures() {
+        List<Facture> factures = new ArrayList<>();
+        // Add some sample factures to the list
 
+        Mockito.when(factureService.retrieveAllFactures()).thenReturn(factures);
+
+        List<Facture> result = factureController.getFactures();
+
+        assertEquals(factures, result);
+    }
+
+    @Test
+    public void testRetrieveFacture() {
+        Long factureId = 1L;
+        Facture facture = new Facture();
+        // Set properties for the facture
+
+        Mockito.when(factureService.retrieveFacture(factureId)).thenReturn(facture);
+
+        Facture result = factureController.retrieveFacture(factureId);
+
+        assertEquals(facture, result);
+    }
+
+    @Test
+    public void testAddFacture() {
+        Facture factureToAdd = new Facture();
+        // Set properties for the facture to add
+
+        Mockito.when(factureService.addFacture(factureToAdd)).thenReturn(factureToAdd);
+
+        Facture result = factureController.addFacture(factureToAdd);
+
+        assertEquals(factureToAdd, result);
+    }
+}
